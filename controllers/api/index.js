b3e52c7d2147ce610befbe06e5328bf7e7e14719
romeo <rdumlao07@gmail.com>
2023-12-04 20:20:11 -0500

    update added favorites route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,7 +7,7 @@ router.use('/users', userRoutes);
 router.use('/dishes', dishRoutes);
 router.use('/reviews', reviewRoutes);
 
-module.exports = router;const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
 const dishRoutes = require('./dishRoutes');
 const reviewRoutes = require('./reviewRoutes');
